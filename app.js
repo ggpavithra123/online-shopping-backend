43b52bf40d309c51970e86dc8e704e2af8b33a72
ggpavithra123 <158811352+ggpavithra123@users.noreply.github.com>
2026-02-23 13:00:08 +0530

    Update CORS origin to include production URL

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cookieParser = require('cookie-parser');
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","http://127.0.0.1:3000"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://onlineshoppingfrontend.vercel.app","http://127.0.0.1:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
