7314d5b1f43f84ee297e8fbf44695fe650c07df4
ggpavithra123 <158811352+ggpavithra123@users.noreply.github.com>
2026-02-23 17:37:55 +0530

    Refactor quotes and update CORS origin list

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const productRoutes = require('./routes/product'); 
-const authRoutes = require('./routes/auth');  
-const order = require('./routes/order')
-const errorMiddleware = require('./middlewares/error');
-const cookieParser = require('cookie-parser');
+const productRoutes = require("./routes/product");
+const authRoutes = require("./routes/auth");
+const order = require("./routes/order");
+const errorMiddleware = require("./middlewares/error");
+const cookieParser = require("cookie-parser");
 //const fileUpload = require("express-fileupload");
 
 //app.use(express.json());
 
-
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://onlineshoppingfrontend.vercel.app","https://online-shopping-frontend-xi.vercel.app/","http://127.0.0.1:3000"],
+    origin: [
+      "https://onlineshoppingfrontend.vercel.app",
+      "https://online-shopping-frontend-xi.vercel.app/",
+      "http://localhost:3001",
+      "http://127.0.0.1:3000",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-  })
+  }),
 );
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(fileUpload());
-app.use('/api/v1', productRoutes);
-app.use('/api/v1', authRoutes);
-app.use('/api/v1', order);
-
+app.use("/api/v1", productRoutes);
+app.use("/api/v1", authRoutes);
+app.use("/api/v1", order);
 
 app.use(errorMiddleware);
 //app.use(express.urlencoded({ extended: true }))
-
 
 module.exports = app;
