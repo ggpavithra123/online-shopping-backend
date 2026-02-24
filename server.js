9649895d61b1f8b954f27c3e083289e28898d1f2
ggpavithra123 <ggpavi12@gmail.com>
2026-02-24 15:09:50 +0530

    server.js file

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
-require('dotenv').config({ path: './config/config.env' });
+require('dotenv').config();
+
 const app = require('./app');
-const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
-
-
-// Setting up config file
-//dotenv.config({ path: 'backend/config/config.env' });
 
 connectDatabase();
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+const PORT = process.env.PORT || 8000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is working on port ${PORT}`);
 });
-
-process.on('unhandledRejection', (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log('Shutting down the server due to Unhandled Promise Rejection'); 
-    server.close(() => {
-        process.exit(1);
-    });
-});
-
-
-
-process.on('uncaughtException',(err)=>{
-    console.log(`Error: ${err.message}`);
-    console.log('Shutting down the server due to uncaught exception error');
-    server.close(()=>{
-        process.exit(1);
-    })
-});
-
-//console.log(a);
