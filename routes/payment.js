29da308eefba0e51738d410d0dd6855f0b0b559e
ggpavithra123 <ggpavi12@gmail.com>
2026-02-25 15:13:06 +0530

    isAuthenticatedUser added

--- routes/payment.js
+++ routes/payment.js
@@ -3,7 +3,7 @@ const { processPayment, sendStripeApi } = require('../controllers/paymentControl
 const { isAuthenticatedUser } = require('../middlewares/authenticate');
 const router = express.Router();
 
-router.route('/payment/process').post(  processPayment);
+router.route('/payment/process').post( isAuthenticatedUser, processPayment);
 router.route('/stripeapi').get( isAuthenticatedUser, sendStripeApi);
 
 
