00f5aaae60a12db4e3b5dce3a2ec1135f07e5ba7
ggpavithra123 <ggpavi12@gmail.com>
2026-02-25 13:43:14 +0530

    payment route updated

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const productRoutes = require("./routes/product");
 const authRoutes = require("./routes/auth");
+const paymentRoutes = require("./routes/payment");
 const order = require("./routes/order");
 const errorMiddleware = require("./middlewares/error");
 const cookieParser = require("cookie-parser");
@@ -32,6 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", order);
+app.use("/api/v1", paymentRoutes);
 
 app.use(errorMiddleware);
 //app.use(express.urlencoded({ extended: true }))
