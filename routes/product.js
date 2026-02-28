7124f31337efd4c732fd2974acf5e10476dd488d
ggpavithra123 <ggpavi12@gmail.com>
2026-02-28 16:26:18 +0530

    Multer steps pathname change fix

--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 
 const upload = multer({storage: multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, path.join( __dirname,'..' , 'uploads/product' ) )
+        cb(null, path.join( __dirname,'..' , 'uploads/user' ) )
     },
     filename: function(req, file, cb ) {
         cb(null, file.originalname)
